dcd01e1c23f34020f068893d0aa5c05b55464994
wufeng321 <48675419+wufeng321@users.noreply.github.com>
2024-04-25 14:19:49 +0800

    整合interface模拟接口模块和sdk模块

---
+++ wuapi-client-sdk/README.md
@@ -0,0 +1,18 @@
+#SDK模块
+```aidl
+<groupId>com.wufeng</groupId>
+<artifactId>wuapi-client-sdk</artifactId>
+<version>0.0.1</version>
+<name>wuapi-client-sdk</name>
+```
+
+使用该SDK，在你的代码中添加以下依赖：
+
+```xml
+ <dependency>
+    <groupId>com.wufeng</groupId>
+    <artifactId>wuapi-client-sdk</artifactId>
+    <version>0.0.1</version>
+ </dependency>
+```
+
---
+++ wuapi-interface/src/main/java/com/wufeng/wuapiinterface/controller/NameController.java
@@ -0,0 +1,49 @@
+package com.wufeng.wuapiinterface.controller;
+
+import com.wufeng.wuapiclientsdk.model.User;
+import com.wufeng.wuapiclientsdk.utils.SignUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author wufeng
+ * @date 2024/4/17 15:58
+ * @Description:
+ */
+
+@RestController
+@RequestMapping("/name")
+public class NameController {
+    @GetMapping("/getNameByGet")
+    public String getNameByGet(String name, HttpServletRequest request){
+        System.out.println(request.getHeader("taco"));
+        return "GET 你的名字是"+ name;
+    }
+    @PostMapping("/getNameByPost")
+    public String getNameByPost(@RequestParam String name){
+        return "POST 你的名字是"+ name;
+    }
+    @PostMapping("/getUserNameByPost")
+    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request){
+        //// 从请求头中获取参数
+        //String accessKey = request.getHeader("accessKey");
+        //String nonce = request.getHeader("nonce");
+        //String timeStamp = request.getHeader("timeStamp");
+        //String sign = request.getHeader("sign");
+        //String body = request.getHeader("body");
+        //
+        //// todo: 模拟校验
+        //if(!accessKey.equals("taco")){
+        //    throw new RuntimeException("无权限");
+        //}
+        //
+        //String serveSign = SignUtils.genSign(body, "abcd");
+        //if(!sign.equals(serveSign)){
+        //    throw new RuntimeException("签名验证失败");
+        //}
+
+        String result = "POST 用户名字是"+ user.getUsername();
+        return result;
+    }
+}
